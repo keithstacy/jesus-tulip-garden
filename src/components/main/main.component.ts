015dbe49711ef8adadb72ae32c02f3837cf98c42
Keith Stacy <keith.stacy@live.com>
2023-09-04 07:46:06 -0500

    TS changes, CSS changes

--- src/components/main/main.component.ts
+++ src/components/main/main.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  onMouseEnter(position: number){
+    let theDiv = document.getElementById('div' + position) as HTMLDivElement;
+    theDiv.style.backgroundColor = 'black';
+    theDiv.style.display = 'none';
+  }
 
+  onMouseLeave(position: number){
+    let theDiv = document.getElementById('div' + position) as HTMLDivElement;
+    theDiv.style.display = '';
+  }
 }
