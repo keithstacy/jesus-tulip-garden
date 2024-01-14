e29b1095c5fdf2084c082a49e5e03c023548ee52
Keith Stacy <keith.stacy@live.com>
2024-01-14 17:42:59 -0600

    New scroll system

--- src/components/find-us/find-us.component.ts
+++ src/components/find-us/find-us.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewContainerRef } from '@angular/core';
 import { ContentService } from 'src/services/content.service';
+
+declare var Microsoft: any;
 
 @Component({
   selector: 'app-find-us',
@@ -7,15 +9,23 @@ import { ContentService } from 'src/services/content.service';
   styleUrls: ['./find-us.component.css']
 })
 export class FindUsComponent implements OnInit {
+  latitude: number = 44.7790009;
+  longitude: number = -93.7739879;
+
+  @ViewChild('theMap') theMap: ElementRef = new ElementRef(null);
   
   constructor(private contentSvc: ContentService, private containerRef: ViewContainerRef){}
 
   ngOnInit(): void {
     console.log('Content loaded');
+    this.loadMapScenario();
   }
 
   returnToMain(){
     console.log('returnToMain called')
     this.contentSvc.LoadMainComponent(this.containerRef);
+  }
+
+  loadMapScenario() {
   }
 }
---
+++ src/components/scroll/scroll.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ChurchFamilyComponent } from '../church-family/church-family.component';
+
+@Component({
+  selector: 'app-scroll',
+  templateUrl: './scroll.component.html',
+  styleUrls: ['./scroll.component.css']
+})
+export class ScrollComponent {
+
+}
